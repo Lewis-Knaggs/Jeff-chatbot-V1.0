a4e77f025c254a75e16f04e117561499d7776159
Lewis-Knaggs <127341287+Lewis-Knaggs@users.noreply.github.com>
2023-06-01 17:06:37 +0100

    Update weather.py

--- Jeff Chatbot V1.0/weather.py
+++ Jeff Chatbot V1.0/weather.py
@@ -3,9 +3,9 @@ def getWeather():
     import requests
     import json
     api_key = "ced2064942ce7787c69e26317d70477c"
-    # Type in your local latitude and longitude bellow to get your local weather fron jeff, if you don't it will tell you it from Greenland
-    lat = "81.257705"
-    lon = "-34.803757"
+    # Type in your local latitude and longitude bellow to get your local weather fron Jff
+    lat = "0"
+    lon = "0"
     url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
     response = requests.get(url)
     data = json.loads(response.text)
